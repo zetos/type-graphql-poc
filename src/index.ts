dbb297c1add9afdc19ce837404b7c98aa51a4ca9
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-10 20:01:57 -0300

    Implement express-session with redis.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import {ApolloServer} from 'apollo-server-express';
 import express from 'express';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
 
 import { RegisterResolver } from './modules/user/Register';
+import { redis } from './redis';
+
 
 const server = async () => {
     await createConnection();
@@ -14,9 +18,28 @@ const server = async () => {
     });
 
     const apolloServer = new ApolloServer({
-        schema
+        schema,
+        context: ({req}: any) => ({req})
     });
     const app = express();
+
+    const RedisStore = connectRedis(session);
+
+    app.use(
+        session({
+        store: new RedisStore({
+            client: redis as any,
+        }),
+        name: 'qid',
+        secret: 'hardcodedsecret',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+        },
+    }))
 
     apolloServer.applyMiddleware({app});
 
---
+++ src/redis.ts
@@ -0,0 +1,4 @@
+import Redis from "ioredis";
+
+//Default connection string connects to localhost
+export const redis = new Redis();
