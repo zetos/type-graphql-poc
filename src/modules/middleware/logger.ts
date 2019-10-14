ed6021d25f2f088729a8a9cf54bebea3747075a6
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-13 21:08:34 -0300

    Use middleware in the resolver for custom auth

---
+++ src/modules/middleware/logger.ts
@@ -0,0 +1,7 @@
+import { MiddlewareFn } from "type-graphql";
+import { MyContext } from "src/types/MyContext";
+
+export const logger: MiddlewareFn<MyContext> = async ({args}, next) => {
+    console.log('logger middleware args:', args);
+    return next();
+}
