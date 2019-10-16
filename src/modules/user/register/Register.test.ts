f90b150442c2e5072fd20002d4197d6ddfa3f8a6
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-15 22:27:28 -0300

    First test setup for 'Register' mutation.

---
+++ src/modules/user/register/Register.test.ts
@@ -0,0 +1,44 @@
+import { Connection } from "typeorm";
+
+import { testConn } from "../../../test-utils/testConn";
+import { gCall } from "../../../test-utils/gCall";
+
+let conn: Connection;
+
+beforeAll(async () => {
+    conn = await testConn();
+});
+
+afterAll(async () => {
+    await conn.close();
+});
+
+const registerMutation = `
+mutation Register($data: RegisterInput!) {
+    register(
+        data: $data
+    ) {
+        id
+        firstName
+        lastName
+        email
+        name
+    }
+}
+`;
+
+describe('Register', () => {
+    it('create user', async () => {
+        console.log(await gCall({
+            source: registerMutation,
+            variableValues: {
+                data: {
+                    firstName: 'ada',
+                    lastName: 'lovelace',
+                    email: 'ada@coolada.com',
+                    password: 'qwerty'
+                }
+            }
+        }))
+    })
+})
