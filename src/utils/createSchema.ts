b6dc80656e4823cf67e1cd5008e1e89a9df484e8
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-15 22:46:06 -0300

    Switch createSchema to the old style.
    Changed back to make the ts-node-dev work again.

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -1,9 +1,12 @@
 import { buildSchema } from "type-graphql";
-
-// resolvers: [MeResolver, RegisterResolver, LoginResolver, ConfirmUserResolver, LogoutResolver],
+import { MeResolver } from "../modules/user/Me";
+import { RegisterResolver } from "../modules/user/Register";
+import { LoginResolver } from "../modules/user/Login";
+import { ConfirmUserResolver } from "../modules/user/ConfirmUser";
+import { LogoutResolver } from "../modules/user/Logout";
 
 export const createSchema = () => buildSchema({
-    resolvers: [__dirname + '/../modules/*/*.ts'],
+    resolvers: [MeResolver, RegisterResolver, LoginResolver, ConfirmUserResolver, LogoutResolver],
     authChecker: ({context: {req}}) => {
         return !!req.session.userId;
     }
