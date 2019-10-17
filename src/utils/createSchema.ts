acb0d8b91721d0e1cdcdf76c73e7dea7f4e077a0
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-16 22:52:39 -0300

    add ProfilePictureResolver to schema

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -5,6 +5,7 @@ import { LoginResolver } from "../modules/user/Login";
 import { ConfirmUserResolver } from "../modules/user/ConfirmUser";
 import { LogoutResolver } from "../modules/user/Logout";
 import { CreateUserResolver } from "../modules/user/CreateUser";
+import { ProfilePictureResolver } from "../modules/user/ProfilePicture";
 
 export const createSchema = () => buildSchema({
     resolvers: [
@@ -13,7 +14,8 @@ export const createSchema = () => buildSchema({
         LoginResolver,
         ConfirmUserResolver,
         LogoutResolver,
-        CreateUserResolver
+        CreateUserResolver,
+        ProfilePictureResolver
     ],
     authChecker: ({ context: { req } }) => {
         return !!req.session.userId;
