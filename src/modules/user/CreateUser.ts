6add58aa722d021ef280999fecd7e282730efd3d
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-16 22:11:34 -0300

    Create user resolver with hight order resolver.

--- src/modules/user/CreateUser.ts
+++ src/modules/user/CreateUser.ts
@@ -1,14 +1,34 @@
-import { Arg, Mutation, Resolver } from "type-graphql";
+import { Arg, Mutation, Resolver, ClassType } from "type-graphql";
 
 import { RegisterInput } from "./register/RegisterInput";
 import { User } from "../../entity/User";
 
-@Resolver()
-export class CreateUserResolver {
-    @Mutation(() => User)
-    async createUser(
-        @Arg('data') data: RegisterInput
-    ) {
-        return User.create(data).save()
+function createBaseResolver<T extends ClassType, X extends ClassType>(
+    suffix: string,
+    returnType: T,
+    inputType: X,
+    entity: any
+) {
+    @Resolver({ isAbstract: true })
+    abstract class BaseResolver {
+        @Mutation(() => returnType, {name: `create${suffix}`})
+        async create(@Arg('data', () => inputType) data: any) {
+            return entity.create(data).save();
+        }
+
     }
+
+    return BaseResolver;
+}
+
+const BaseCreateUser = createBaseResolver('User', User, RegisterInput, User);
+
+@Resolver()
+export class CreateUserResolver extends BaseCreateUser{
+    // @Mutation(() => User)
+    // async createUser(
+    //     @Arg('data') data: RegisterInput
+    // ) {
+    //     return User.create(data).save()
+    // }
 }
