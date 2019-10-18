d08725e43b023427fe93e843d802f17d181c0dd1
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-18 00:12:32 -0300

    start of compose-setup script

---
+++ compose-setup.sh
@@ -0,0 +1,17 @@
+# WIP
+# DONT RUN IT YET
+
+# Check docker version
+docker-compose -v
+
+# Start up containers in the background (when its already built)
+docker-compose up -d
+
+# Check docker contrainers
+docker container ls
+
+# Check docker-compose logs
+docker-compose logs
+
+# Shutdown the containers
+docker-compose down
