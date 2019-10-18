11cdd2eddfa0fa4790f2aaab92cd3aee74258a13
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-17 23:30:57 -0300

    add simple retry logic on connection.

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,22 @@ import { redis } from "./redis";
 import { createSchema } from "./utils/createSchema";
 
 const main = async () => {
-  await createConnection();
+
+  let retries = 5;
+  while(retries) {
+    try{
+      await createConnection();
+      break;
+    } catch (err) {
+      console.error(err);
+      retries -= retries;
+      console.log(`retries left: ${retries}`);
+      
+      // wait 5 seconds
+      await new Promise(res => setTimeout(res, 5000));
+    }
+  }
+  
 
   const schema = await createSchema();
 
